4f7c62e4bf3e164a4625eaf2f13fbe242ba20581
Arnaud Gourlay <arnaud.gourlay@commercetools.de>
2014-11-28 14:42:01 +0100

    fix long stream test generation

--- src/test/scala/ApplicationITSpec.scala
+++ src/test/scala/ApplicationITSpec.scala
@@ -21,27 +21,15 @@ class ApplicationITSpec extends FlatSpec with Matchers {
     resultFileContent shouldEqual referenceResult
   }
 
-  // TODO fix stackoverflow in Stream gen.
   def repeatTestFileContent(n: Int): Stream[String] = {
-    @tailrec
-    def loopString(v: Int, s: String, acc: Stream[String]): Stream[String] = {
-      if (v == 0) acc
-      else {
-        val input = if (acc.isEmpty) acc :+ (",") else acc
-        loopString(v - 1, s, input :+ (s))
-      }
-    }
-
     val resultFile = new File(getClass.getResource("/test.json").getPath())
     val twoJsons = FileUtils.readFileToString(resultFile).drop(1).dropRight(1) // remove "[]"
-    val acc = loopString(n, twoJsons, Stream())
-    val stream: Stream[String] = "[" #:: acc :+ ("]")
-    stream
+    "[" #:: Stream.continually[String](twoJsons + ",").take(n) :+ ("]")
   }
 
   "The application" should "not blow up the heap while transforming huge stream" in {
     val resultFileName = "shouldBeDeleted.csv"
-    val inputStream = repeatTestFileContent(2000)
+    val inputStream = repeatTestFileContent(500000)
     Json2CsvStream.streamConversion(inputStream, resultFileName)
     FileUtils.forceDelete(new File(resultFileName))
   }
